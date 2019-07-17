3ca41c6dcc9fccf4d187a1b5805bef9bf19bbac3
Ayan Das <ayan@Home-MBP.attlocal.net>
2019-07-17 19:36:58 -0400

    Changed backend address

--- app/js/factory/GameService.js
+++ app/js/factory/GameService.js
@@ -1,7 +1,7 @@
 angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $q) {
     var getGameInfo = function(id) {
       var deferred = $q.defer();
-      $http.get('https://serene-spire-90540.herokuapp.com/matches/' + id).success(function(data, status, headers, config) {
+      $http.get('https://aqueous-springs-22490.herokuapp.com/matches/' + id).success(function(data, status, headers, config) {
           //console.log("Number of balls: " + data.length);
           deferred.resolve(data);
       }).error(function(data, status, headers, config) {
@@ -13,7 +13,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getPlayers = function() {
       var deferred = $q.defer();
-      $http.get('https://serene-spire-90540.herokuapp.com/players/').success(function(data, status, headers, config) {
+      $http.get('https://aqueous-springs-22490.herokuapp.com/players/').success(function(data, status, headers, config) {
           deferred.resolve(data);
       }).error(function(data, status, headers, config) {
         console.log("Error: " + JSON.stringify(data));
@@ -46,7 +46,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getGames = function() {
         var deferred = $q.defer();
-        $http.get('https://serene-spire-90540.herokuapp.com/matches/').success(function(data, status, headers, config) {
+        $http.get('https://aqueous-springs-22490.herokuapp.com/matches/').success(function(data, status, headers, config) {
             deferred.resolve(data);
         }).error(function(data, status, headers, config) {
           console.log("Error: " + JSON.stringify(data));
@@ -57,7 +57,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getPlayerList = function() {
         var deferred = $q.defer();
-        $http.get('https://serene-spire-90540.herokuapp.com/players/list').success(function(data, status, headers, config) {
+        $http.get('https://aqueous-springs-22490.herokuapp.com/players/list').success(function(data, status, headers, config) {
             deferred.resolve(data);
         }).error(function(data, status, headers, config) {
             console.log("Error: " + JSON.stringify(data));
@@ -68,7 +68,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getBallsByBatsman = function(batsman) {
       var deferred = $q.defer();
-      $http.get('https://serene-spire-90540.herokuapp.com/matches/batsman/' + batsman).success(function(data, status, headers, config) {
+      $http.get('https://aqueous-springs-22490.herokuapp.com/matches/batsman/' + batsman).success(function(data, status, headers, config) {
           deferred.resolve(data);
       }).error(function(data, status, headers, config) {
           console.log("Error: " + JSON.stringify(data));
@@ -79,7 +79,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getBallsByBowler = function(bowler) {
       var deferred = $q.defer();
-      $http.get('https://serene-spire-90540.herokuapp.com/matches/bowler/' + bowler).success(function(data, status, headers, config) {
+      $http.get('https://aqueous-springs-22490.herokuapp.com/matches/bowler/' + bowler).success(function(data, status, headers, config) {
           deferred.resolve(data);
       }).error(function(data, status, headers, config) {
           console.log("Error: " + JSON.stringify(data));
@@ -90,7 +90,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getMatchesByTeam = function(team) {
         var deferred = $q.defer();
-        $http.get('https://serene-spire-90540.herokuapp.com/matches/team/' + team).success(function(data, status, headers, config) {
+        $http.get('https://aqueous-springs-22490.herokuapp.com/matches/team/' + team).success(function(data, status, headers, config) {
             deferred.resolve(data);
         }).error(function(data, status, headers, config) {
             console.log("Error: " + JSON.stringify(data));
@@ -101,7 +101,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getPlayersByTeam = function(team) {
         var deferred = $q.defer();
-        $http.get('https://serene-spire-90540.herokuapp.com/players/team/' + team).success(function(data, status, headers, config) {
+        $http.get('https://aqueous-springs-22490.herokuapp.com/players/team/' + team).success(function(data, status, headers, config) {
             deferred.resolve(data);
         }).error(function(data, status, headers, config) {
             console.log("Error: " + JSON.stringify(data));
@@ -112,7 +112,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getPlayerGraph = function() {
       var deferred = $q.defer();
-      $http.get('https://serene-spire-90540.herokuapp.com/players/graph/').success(function(data, status, headers, config) {
+      $http.get('https://aqueous-springs-22490.herokuapp.com/players/graph/').success(function(data, status, headers, config) {
           deferred.resolve(data);
       }).error(function(data, status, headers, config) {
           console.log("Error: " + JSON.stringify(data));
@@ -123,7 +123,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getPartnerships = function(id) {
       var deferred = $q.defer();
-      $http.get('https://serene-spire-90540.herokuapp.com/matches/' + id + "/partnerships").success(function(data, status, headers, config) {
+      $http.get('https://aqueous-springs-22490.herokuapp.com/matches/' + id + "/partnerships").success(function(data, status, headers, config) {
           deferred.resolve(data);
       }).error(function(data, status, headers, config) {
           console.log("Error: " + JSON.stringify(data));
@@ -145,7 +145,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getAllBalls = function() {
         var deferred = $q.defer();
-        $http.get('https://serene-spire-90540.herokuapp.com/matches/allBalls').success(function(data, status, headers, config) {
+        $http.get('https://aqueous-springs-22490.herokuapp.com/matches/allBalls').success(function(data, status, headers, config) {
             deferred.resolve(data);
         }).error(function(data, status, headers, config) {
             console.log("Error: " + JSON.stringify(data));
@@ -156,7 +156,7 @@ angular.module('myApp').factory('GameService', ['$http', '$q', function($http, $
 
     var getBallsByRegion = function(params) {
         var deferred = $q.defer();
-        var url = 'https://serene-spire-90540.herokuapp.com/balls/?'
+        var url = 'https://aqueous-springs-22490.herokuapp.com/balls/?'
             + 'leftX=' + params.leftX + "&"
             + 'rightX=' + params.rightX + "&"
             + 'topY=' + params.topY + "&"
